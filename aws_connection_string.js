8c36e526beaf361e722fc9036fe826ee5089876d
tanguturihome-bit <tanguturihome@gmail.com>
2026-01-30 20:29:27 -0500

    Update aws_connection_string.js

--- aws_connection_string.js
+++ aws_connection_string.js
@@ -3,7 +3,3 @@ const AWS_CONFIG = {
   aws_secret_access_key: "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY",
   region: "us-east-1"
 };
-
-// Alternate format (env-style)
-process.env.AWS_ACCESS_KEY_ID = "ASIAQWER1234EXAMPLE";
-process.env.AWS_SECRET_ACCESS_KEY = "ABCD1234abcdEFGH5678ijklMNOPQRSTuvWX+/=";
